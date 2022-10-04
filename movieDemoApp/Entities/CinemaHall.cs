1d5d9cf47c0fa6bb8f5ef5a19728d4b7b763adcd
MallikarjungoudA <aamallikarjuna18@gmail.com>
2022-10-04 19:45:38 +0530

    Enum exaple

--- movieDemoApp/Entities/CinemaHall.cs
+++ movieDemoApp/Entities/CinemaHall.cs
@@ -1,9 +1,13 @@
-﻿namespace movieDemoApp.Entities
+﻿using movieDemoApp.Enums;
+
+namespace movieDemoApp.Entities
 {
     public class CinemaHall
     {
 
         public int Id { get; set; }
+
+        public CinemaHallType CinemaHallTypes { get; set; }
 
         //[Precision(precision:9, scale:2)]
         public decimal cost { get; set; }
