34c535e543a33bcd699cf407af3d531e89a1e777
MallikarjungoudA <aamallikarjuna18@gmail.com>
2022-10-24 13:16:27 +0530

    fetching genres data using async and await

---
+++ movieDemoApp/Controllers/GenresController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using movieDemoApp.Entities;
+
+namespace movieDemoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly MovieDbContext _context;
+
+        public GenresController(MovieDbContext mdbcontext)
+        {
+            this._context = mdbcontext;
+        } 
+
+        public async Task<IEnumerable<Genre>> Get()
+        {
+            return await _context.genres.ToListAsync();
+        }
+    }
+}
